a60f7c6d89a5a0f8f8590ca9836c0d511099e063
ToshiyaAndo <sagittarius30008@gmail.com>
2021-06-15 12:37:14 +0900

    js modified

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ $("#target").skippr({
   // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
   hidePrevious : false
 });
+
+var countdown = function(due){
+  var now = new Date();
+
+  var rest = due.getTime() - now.getTime();
+  var sec = Math.floor(rest/1000%60);
+  var min = Math.floor(rest/1000/60)%60;
+  var hours = Math.floor(rest/1000/60/60)%24;
+  var days = Math.floor(rest/1000/60/60/24);
+  var count = [days,hours,min,sec];
+
+  return count;
+}
+
+var goal = new Date(2021,5,24,13);
+
+var recalc = function(){
+  var counter = countdown(goal);
+  document.getElementById('day').textContent = counter[0];
+  document.getElementById('hour').textContent = counter[1];
+  document.getElementById('min').textContent = counter[2];
+  document.getElementById('sec').textContent = counter[3];
+  refresh();
+}
+
+var refresh = function(){
+  setTimeout(recalc,1000);
+}
+recalc();
